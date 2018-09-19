973e83b12a37457a84c893befe0911ab0481f98a
josselin <douineau.2611@apside-groupe.com>
2018-09-19 15:27:12 +0200

    TestJsf fonctionne

--- TestJSF/TestJSF/src/com/journaldev/springhibernate/model/Person.java
+++ TestJSF/TestJSF/src/com/journaldev/springhibernate/model/Person.java
@@ -1,34 +1,30 @@
 package com.journaldev.springhibernate.model;
 
-import javax.faces.bean.ManagedBean;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
- 
-/**
- * Entity bean with JPA annotations
- * Hibernate provides JPA implementation
- * @author pankaj
- *
- */
-@SuppressWarnings("deprecation")
-@Entity
-@Table(name="PERSON")
-@ManagedBean(name="person")
 public class Person {
-	@Id
-    @Column(name="id")
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+	
     private int id;
      
     private String name;
      
     private String country;
  
-    public int getId() {
+    public Person(int id, String name, String country) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.country = country;
+	}
+
+    public Person(int id) {
+		super();
+		this.id = id;
+	}
+    
+	public Person() {
+		super();
+	}
+
+	public int getId() {
         return id;
     }
  
--- TestJSF/TestJSF/src/com/journaldev/springhibernate/service/PersonService.java
+++ TestJSF/TestJSF/src/com/journaldev/springhibernate/service/PersonService.java
@@ -1,12 +1,49 @@
 package com.journaldev.springhibernate.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.journaldev.springhibernate.model.Person;
- 
-public interface PersonService {
- 
-    public void addPerson(Person p);
-    public List<Person> listPersons();
-     
+
+public class PersonService {
+
+	private final static String[] names;
+	private final static String[] countries;
+	private static List<Person> persons = new ArrayList<Person>();
+	private static int uniqueId;
+
+
+    static {
+    	names = new String[3];
+    	names[0]= "Joss";
+    	names[1]= "John";
+    	names[2]= "Mario";
+    	
+    	countries = new String[3];
+    	countries[0]= "FR";
+    	countries[1]= "GB";
+    	countries[2]= "IT";
+    }
+    
+    
+    public void createPersons(int size) {
+    	for(int i = 0 ; i < size ; i++) {
+    		persons.add(new Person(i, names[i], countries[i]));
+    	}
+    	uniqueId = size;
+    }
+    
+    public void addPerson(Person p) {
+    	p.setId(++uniqueId);
+    	persons.add(p);
+    }
+
+	public static int getUniqueId() {
+		return uniqueId;
+	}
+
+	public List<Person> getPersons() {
+		return persons;
+	}
+    
 }
